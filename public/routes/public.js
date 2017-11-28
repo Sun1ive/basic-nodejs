11eb0a0ff00179f148764d9f3179c4e9c73d586f
Sergey <sunliveua@gmail.com>
2017-11-28 14:04:29 +0200

    added routes

--- public/routes/public.js
+++ public/routes/public.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 function valid(req, res) {
   const extension = path.extname(req.url);
-  
+  const filename = req.url.slice(1);
+
   let contentType = '';
 
   switch (extension) {
@@ -23,14 +24,16 @@ function valid(req, res) {
       contentType = 'text/plain';
   }
 
-  
+  res.statusCode = 200;
+  res.setHeader('Content-Type', contentType);
 
-  const stream = fs.createReadStream(path.join(__dirname, req.url));
+  const stream = fs.createReadStream(path.resolve('public', filename));
 
   stream.pipe(res);
 
   stream.on('error', error => {
     if (error.code === 'ENOENT') {
+      console.log(error);
       res.writeHead(404, { 'Content-type': 'text/plain' });
       res.end('Not Found');
     } else {
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
 const valid = require('./public/routes/public');
+const home = require('./public/routes/home');
 
 http
   .createServer((req, res) => {
     if (req.url.match(/\.(html|css|js|png|jpg)$/)) {
       valid(req, res);
     } else if (req.url === '/') {
+      home(req, res);
     } else if (req.url.startsWith('/search')) {
     } else {
     }
